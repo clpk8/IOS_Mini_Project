b6ebe492b7fc55c7b22124c27d8c13da7381fcc9
Chunbin lin <chunbinlin@Chunbins-MacBook-Pro.local>
2018-08-28 23:11:36 -0500

    First commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Chunbin lin on 8/28/18.
+//  Copyright © 2018 Chunbin lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var diamonImg: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
